23f0b071932e4266d35a7e689cbee9cdc99b4142
ChandanNaik999 <chandannaik999@gmail.com>
2019-11-09 16:40:03 +0530

    Remove Ignore files

--- build/build-status.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-# Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/ns3.29-tcp-variants-comparison-debug']
-
-# Scripts that are runnable.
-ns3_runnable_scripts = []
-
